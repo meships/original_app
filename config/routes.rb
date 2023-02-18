d26f9242fc199560d7ad533a0eb6d9b52b4f9072
qz <meshipsmou@yahoo.co.jp>
2023-02-19 04:38:50 +0900

    daily_reportコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'daily_reports/index'
+  get 'daily_reports/new'
+  get 'daily_reports/show'
+  get 'daily_reports/edit'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
 end
