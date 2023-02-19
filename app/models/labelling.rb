4446961b6b9120ebd69747d372b0f8c1bbe8f2aa
qz <meshipsmou@yahoo.co.jp>
2023-02-19 14:59:40 +0900

    中間テーブルlabellingモデルdb作成

---
+++ app/models/labelling.rb
@@ -0,0 +1,4 @@
+class Labelling < ApplicationRecord
+  belongs_to :daily_report
+  belongs_to :label
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_19_055239) do
+ActiveRecord::Schema.define(version: 2023_02_19_055711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 2023_02_19_055239) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_daily_reports_on_user_id"
+  end
+
+  create_table "labellings", force: :cascade do |t|
+    t.bigint "daily_report_id", null: false
+    t.bigint "label_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["daily_report_id"], name: "index_labellings_on_daily_report_id"
+    t.index ["label_id"], name: "index_labellings_on_label_id"
   end
 
   create_table "labels", force: :cascade do |t|
@@ -46,4 +55,6 @@ ActiveRecord::Schema.define(version: 2023_02_19_055239) do
   end
 
   add_foreign_key "daily_reports", "users"
+  add_foreign_key "labellings", "daily_reports"
+  add_foreign_key "labellings", "labels"
 end
