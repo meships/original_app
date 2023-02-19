ef688e967c48baa21c3af2697d6c552d0208f96d
qz <meshipsmou@yahoo.co.jp>
2023-02-19 14:33:31 +0900

    admin,ゲスト用アクセス制限作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
-  skip_before_action :login_required, only: [:new, :create]
+  skip_before_action :login_required, only: [:new, :create, :guest_login, :admin_guest_login]
   def new
+    redirect_to user_path(current_user.id) if logged_in?
   end
 
   def create
@@ -20,4 +21,25 @@ class SessionsController < ApplicationController
     redirect_to new_session_path
   end
 
+
+
+
+  def guest_login
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.name = 'Guest User'
+      user.password = SecureRandom.urlsafe_base64
+    end
+    log_in user
+    redirect_to daily_reports_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+
+  def admin_guest_login
+    user = User.find_or_create_by!(email: 'admin_guest@example.com') do |user|
+      user.name = 'Guest Admin User'
+      user.password = SecureRandom.urlsafe_base64
+      user.admin = true
+    end
+    log_in user
+    redirect_to admin_users_path, notice: 'ゲスト管理者としてログインしました。'
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,22 @@
 module SessionsHelper
     def current_user
         @current_user ||= User.find_by(id: session[:user_id])
-    end
-  
-    def logged_in?
+      end
+        
+      def logged_in?
         current_user.present?
-    end
+      end
+        
+      # def admin?
+      #   current_user && current_user.admin
+      # end
+    
+      def admin?
+        current_user&.admin?
+      end
+    
+      
+      def log_in(user)
+        session[:user_id] = user.id
+      end
 end
