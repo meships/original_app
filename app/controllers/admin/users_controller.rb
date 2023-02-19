4e948abb08aedaf52be845dec7a6607a76438d50
qz <meshipsmou@yahoo.co.jp>
2023-02-19 14:15:29 +0900

    adminアクション作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,58 @@
 class Admin::UsersController < ApplicationController
+    before_action :admin_user
+
+    def index
+        @users = User.all.includes(:daily_reports)
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+          redirect_to user_path(@user.id)
+        else
+          render :new
+        end
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:notice] = "情報を編集しました！"
+            redirect_to tasks_path
+        else
+            render :edit
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        flash[:notice] = "ユーザーを削除しました"
+        redirect_to admin_users_path
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
+    end
+
+    def admin_user
+      unless logged_in? && current_user.admin?
+          flash[:notice] = "管理者以外はアクセス不可"
+          redirect_to new_session_path
+      end
+    end
 end
