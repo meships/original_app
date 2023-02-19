14b3b7da7d248268a7bfe9c12e78af27a40ebe8c
qz <meshipsmou@yahoo.co.jp>
2023-02-19 19:57:44 +0900

    コメントcreateアクション作成

--- app/controllers/photo_comments_controller.rb
+++ app/controllers/photo_comments_controller.rb
@@ -1,7 +1,38 @@
 class PhotoCommentsController < ApplicationController
+  before_action :login_required
+  # コメントを保存、投稿するためのアクションです。
+  def create
+    # Photoをパラメータの値から探し出し,Photoに紐づくphoto_commentsとしてbuildします。
+    @photo = Photo.find(params[:photo_id])
+    @photo_comment = @photo.photo_comments.build(photo_comment_params)
+    # クライアント要求に応じてフォーマットを変更
+    respond_to do |format|
+      if @photo_comment.save
+        format.js { render :index }
+        #format.html { redirect_to blog_path(@photo) }
+      else
+        format.html { redirect_to blog_path(@photo), notice: '投稿できませんでした...' }
+      end
+    end
+  end
+
   def show
   end
 
   def edit
+  end
+
+  private
+  # ストロングパラメーター
+  def photo_comment_params
+    params.require(:photo_comment).permit(:comment)
+  end
+  # def set_blog
+  #   @photo = Photo.find(params[:photo_id])
+  # end
+
+  #current_user以外はログイン画面に戻す
+  def login_required
+    redirect_to new_session_path unless current_user
   end
 end
