1ee46885a441017036086951ec674b9838026fc4
qz <meshipsmou@yahoo.co.jp>
2023-02-19 21:34:31 +0900

    コメント機能のindex.js.erbが無かったため修正

--- app/controllers/photo_comments_controller.rb
+++ app/controllers/photo_comments_controller.rb
@@ -68,5 +68,4 @@ class PhotoCommentsController < ApplicationController
   def login_required
     redirect_to new_session_path unless current_user
   end
-  
 end
--- app/models/labelling.rb
+++ app/models/labelling.rb
@@ -1,4 +1,7 @@
 class Labelling < ApplicationRecord
   belongs_to :daily_report
   belongs_to :label
+
+  #修正案
+  has_many :daily_reports, through: :labellings
 end
